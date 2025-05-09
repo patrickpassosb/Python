9979d849f250bc025f4bf8f4f3bd15c09ee69ad0
Patrick Passos <patrickcuco20@gmail.com>
2025-05-08 21:22:17 -0300

    I changed the review.py file continuing the course, implementing a side bar that control the price of the books that you're seeing.

--- review.py
+++ review.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+
 st.set_page_config(page_title="Book Review", page_icon=":book:", layout="wide")
 
 st.title("Book Review")
 st.subheader("Customer Reviews")
+
 df_reviews = pd.read_csv('customer reviews.csv')
 df_top100_books = pd.read_csv('Top-100 Trending Books.csv')
 
-df_reviews
-df_top100_books
+price_max = df_top100_books['book price'].max()
+price_min = df_top100_books['book price'].min()
+
+max_price = st.sidebar.slider("Select the price range", min_value=price_min , max_value=price_max, value=(price_min, price_max))
+df_books = df_top100_books[(df_top100_books['book price'] >= max_price[0]) & (df_top100_books['book price'] <= max_price[1])]
+df_books
